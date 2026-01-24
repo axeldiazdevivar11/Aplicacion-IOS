8be017c221d23b37f61d5cc7bdf275ce122ec387
axeldiazdevivar11 <axeldiazdevivar1118@gmail.com>
2026-01-23 22:11:25 -0600

    Initial Commit

---
+++ Ejercicio 1/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Ejercicio 1
+//
+//  Created by Axel Vargas Díaz de Vivar on 23/01/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Ejercicio 1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Ejercicio 1
+//
+//  Created by Axel Vargas Díaz de Vivar on 23/01/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
